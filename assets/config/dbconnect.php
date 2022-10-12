bc312ed5dffd320a4985b261c40e3cbe7a0b2c59
Abdulwahab <disu.abdulwahab@gmail.com>
2022-10-12 13:29:57 +0100

    updated oneline server

--- assets/config/dbconnect.php
+++ assets/config/dbconnect.php
@@ -1,15 +1,15 @@
 <?php
 // Local
-    $server = "localhost";
-    $dbusername = "root";
-    $dbpassword = "";
-    $dbname = "early_money";
+$server = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "early_money";
 
 // Online
-    // $server = "localhost";
-    // $dbusername = "u162278070_early";
-    // $dbpassword = "0>?y!ZOZn";
-    // $dbname = "u162278070_earlymoney";
+// $server = "localhost";
+// $dbusername = "id19689206_u162278070_early";
+// $dbpassword = "HvQyo8G|%xN437~W";
+// $dbname = "id19689206_u162278070_earlymoney";
 
-    
-    $connectDB =  mysqli_connect($server,$dbusername,$dbpassword,$dbname);
+
+$connectDB =  mysqli_connect($server, $dbusername, $dbpassword, $dbname);
